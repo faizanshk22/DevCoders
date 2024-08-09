ac09a9fe7c6cb41953b81c828a614fe1d499e11a
faizanshk22 <faizanshk20@gmail.com>
2024-08-09 22:31:14 +0500

    fixed animation

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import Loader from '/loader.png';
 
 function Preloader() {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-[#1e1e22] z-50">
-      <div className="animate-spin">
+      <motion.div
+        animate={{ rotate: [0, 180] }}
+        transition={{
+          duration: 1,
+          ease: 'linear',
+          repeat: Infinity,
+          repeatType: 'mirror', 
+        }}
+      >
         <img src={Loader} alt="Loading" className="w-32 h-32 sm:w-60 sm:h-60" />
-      </div>
+      </motion.div>
     </div>
   );
 }
