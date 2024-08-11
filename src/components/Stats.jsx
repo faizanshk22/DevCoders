229883ec9c1ec08a063d46867afcb53eeddcdfbf
faizanshk22 <faizanshk20@gmail.com>
2024-08-12 01:51:57 +0500

    button fixed on landing page

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -29,16 +29,16 @@ function Stats() {
 
     return (
         <div className='container ml-4 mt-[10vh] pt-2 xl:pt-0 pb-2 xl:pb-0'>
-            <div className='flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none'>
+            <div className='flex flex-wrap gap-4 max-w-[80vw] mx-auto xl:max-w-none'>
                 {stats.map((item, index) => (
                     <div 
-                        className='flex-1 flex gap-4 items-center justify-center xl:justify-start bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-4 md:p-6 shadow-lg w-full sm:w-auto' 
+                        className='flex-1 flex gap-3 items-center justify-center xl:justify-start bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-3 md:p-4 shadow-lg w-full sm:w-auto sm:max-w-[45%] lg:max-w-[30%]' 
                         key={index}
                     >
                         {startAnimation && (
-                            <CountUp end={item.num} duration={5} delay={2} className='text-3xl sm:text-4xl xl:text-6xl text-white font-extrabold' />
+                            <CountUp end={item.num} duration={5} delay={2} className='text-2xl sm:text-3xl xl:text-5xl text-white font-extrabold' />
                         )}
-                        <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80 text-sm sm:text-base xl:text-lg`}>
+                        <p className={`${item.text.length < 15 ? "max-w-[80px]" : "max-w-[120px]"} leading-snug text-white/80 text-xs sm:text-sm xl:text-base`}>
                             {item.text}
                         </p>
                     </div>
